e54581f8a17ef1d9fb815794d63cbe4a4521ec56
Harsh <hkundnani@gmail.com>
2019-06-27 17:57:05 -0400

    Art using canvas-sketch

---
+++ sketch1.js
@@ -0,0 +1,45 @@
+const canvasSketch = require('canvas-sketch');
+const { lerp } = require('canvas-sketch-util/math');
+const random = require('canvas-sketch-util/random');
+
+const settings = {
+  dimensions: [2048, 2048]
+};
+
+const sketch = () => {
+  const createGrid = () => {
+    const points = [];
+    const count = 50;
+    for (let x = 0; x < count; x++) {
+      for (let y = 0; y < count; y++) {
+        const u = count <= 1 ? 0.5 : x / (count - 1);
+        const v = count <= 1 ? 0.5 : y / (count - 1);
+        points.push([u, v]);
+      }
+    }
+    return points;
+  }
+
+  random.setSeed(512);
+  const points = createGrid().filter(() => random.value() > 0.5);
+  const margin = 300;
+
+  return ({ context, width, height }) => {
+    context.fillStyle = '#ff8282';
+    context.fillRect(0, 0, width, height);
+
+    points.forEach(([u, v]) => {
+      const x = lerp(margin, width - margin, u);
+      const y = lerp(margin, height - margin, v);
+
+      context.beginPath();
+      context.arc(x, y, 10, 0, Math.PI * 2, false);
+      context.lineWidth = 8;
+      context.fillStyle = "#fff"
+      context.fill();
+
+    })
+  };
+};
+
+canvasSketch(sketch, settings);
